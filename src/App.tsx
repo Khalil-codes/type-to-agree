5926c4445c90a7d43dd027fabd2cd0841233c474
Khalil <khalilpati55@gmail.com>
2024-10-11 01:15:46 +0530

    Initialised Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Button variant="default">Button</Button>
     </div>
   );
 }
