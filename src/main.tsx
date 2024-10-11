e6b431286d703ee07d1d466071baa24f09598f8c
Khalil <khalilpati55@gmail.com>
2024-10-11 11:14:47 +0530

    Add themes in the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { ModeToggle } from "./components/theme-toggle.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark">
+      <App />
+      <div className="absolute right-4 top-4">
+        <ModeToggle />
+      </div>
+    </ThemeProvider>
+  </StrictMode>
+);
